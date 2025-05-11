1f47cc688a1a536fc0a28ef38342df45401cfb32
AppsForgeDev <admin@appsforge.dev>
2025-05-11 14:29:23 +0500

    Add app icon and main project hierarchy directories

---
+++ FocusForge/FocusForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusForgeApp.swift
+//  FocusForge
+//
+//  Created by Shcherbinin Andrey on 11.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusForgeTests/FocusForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusForgeTests.swift
+//  FocusForgeTests
+//
+//  Created by Shcherbinin Andrey on 11.05.2025.
+//
+
+import Testing
+@testable import FocusForge
+
+struct FocusForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
